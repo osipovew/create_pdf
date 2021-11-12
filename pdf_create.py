0b028727a741d88db08ea94aaf279f0a43e28794
osipovew <osipovew@gmail.com>
2021-11-12 15:52:20 +0300

    add image

--- pdf_create.py
+++ pdf_create.py
@@ -1,20 +1,20 @@
 from fpdf import FPDF
+import os
 
 # time_txt = open(r"C:\Users\Adm\PycharmProjects\create_pdf", "r") запуск с правами админа
 time_txt = open("text.txt", "r")
 data = time_txt.read()
 time_txt.close()
 
+pdf = FPDF()
+pdf.add_page()
+pdf.image("pic1.jpg")
+pdf.image("pic2.jpg")
+pdf.image("pic3.jpg")
+pdf.set_font("Arial", size=12)
+pdf.ln(85)  # ниже на 85
+pdf.cell(200, 10, txt=data.format("pic1.jpg"), ln=1)
+pdf.output("add_image.pdf")
 
-def add_image(image_path):
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.image(image_path, x=10, y=8, w=100)
-    pdf.set_font("Arial", size=12)
-    pdf.ln(85)  # ниже на 85
-    pdf.cell(200, 10, txt=data.format(image_path), ln=1)
-    pdf.output("add_image.pdf")
-
-
-if __name__ == '__main__':
-    add_image('pic1.jpg')
+arr = os.listdir()
+print(arr)
