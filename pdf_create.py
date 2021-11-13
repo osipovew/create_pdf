06db1c91d0ce7a5aec23aea645ce442021e1446c
osipovew <65f272E5q!>
2021-11-14 02:01:02 +0300

    final

--- pdf_create.py
+++ pdf_create.py
@@ -11,22 +11,21 @@ waveFile = 'wave.png'
 
 pdf = FPDF(orientation='l')
 pdf.set_font('Arial', 'B', 16)
-pdf.add_page()
 
 
-counter = 0
+counter = 1
 
 
 for imageFile in imageList:
     counter = counter + 1
-
+    pdf.add_page()
     pdf.image(waveFile, 0, 0, 297, 20)
 
     txt_content = "PHOTO #" + str(counter) + " | " + open(imageFile + ".txt", "r").read()
 
     pdf.image(imageFile, 10, 30, 277, 170)
     pdf.cell(20, 20, txt_content, 10)
-    pdf.add_page()
+
 
 
 pdf.output("add_image.pdf")
--- test.py
+++ test.py
@@ -6,27 +6,13 @@ arr_jpg = [f for f in glob.glob("*.jpg")]
 imageList = sorted(arr_jpg)
 
 
-waveFile = 'wave.png'
-
-
 pdf = FPDF(orientation='l')
-pdf.set_font('Arial', 'B', 16)
-pdf.add_page()
-
-
-counter = 0
 
 
 for imageFile in imageList:
-    counter = counter + 1
-
-    pdf.image(waveFile, 0, 0, 297, 20)
-
-    txt_content = "PHOTO #" + str(counter) + " | " + open(imageFile + ".txt", "r").read()
-
-    pdf.image(imageFile, 10, 30, 277, 170)
-    pdf.cell(20, 20, txt_content, 10)
     pdf.add_page()
+    pdf.image(imageFile, 10, 30, 277, 170)
+
 
 
 pdf.output("add_image.pdf")
