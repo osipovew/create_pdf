ac1cd25e97d7f8bd4e83b6bf577421aef1e632ab
osipovew <65f272E5q!>
2021-11-13 19:53:44 +0300

    cell() траблы

--- pdf_create.py
+++ pdf_create.py
@@ -1,8 +1,6 @@
 from fpdf import FPDF
-import os
 import glob
 from PIL import Image
-
 
 arr_jpg = [f for f in glob.glob("*.jpg")]
 
@@ -14,24 +12,16 @@ time_txt = open("text.txt", "r")
 data = time_txt.read()
 time_txt.close()
 
-
-
 pdf = FPDF(orientation='l')
-pdf.add_page()
+
 for imageFile in imagelist:
     cover = Image.open(imageFile)
     width, height = cover.size
-    pdf.image(imageFile, 0, 0, width, height)
+    pdf.add_page()
+    pdf.image(imageFile, 0, 0)
+    pdf.cell(100, 10, txt=data.format(imageFile), ln=1) #пошел говорит в жопу
 
-    width, height = float(width * 0.264583), float(height * 0.264583)
+
 pdf.set_font("Arial", size=25)
-# pdf.ln(85)  # ниже на 85
-#pdf.cell(100, 10, txt=data.format("pic1.jpg"), ln=1)
-pdf_size = {'P': {'w': 210, 'h': 297}, 'L': {'w': 297, 'h': 210}}
-
+pdf.ln(100)  # ниже на 85
 pdf.output("add_image.pdf")
-
-
-
-
-
