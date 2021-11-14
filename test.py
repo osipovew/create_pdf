dd8c1eb3eb1c62764c9f5b805ef99eb4439a6e9f
osipovew <65f272E5q!>
2021-11-14 04:15:37 +0300

    final

--- test.py
+++
@@ -1,8 +0,0 @@
-from fpdf import FPDF
-pdf = FPDF()
-imagelist = "lg.png"
-# imagelist is the list with all image filenames
-for image in imagelist:
-    pdf.add_page()
-    pdf.image(imagelist, 10, 10, 10, 10)
-pdf.output("yourfile.pdf", "F")
