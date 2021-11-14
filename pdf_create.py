f4833aae73386461d5cb2c607e98aead8d377219
osipovew <65f272E5q!>
2021-11-14 04:06:19 +0300

    final

--- pdf_create.py
+++ pdf_create.py
@@ -1,5 +1,4 @@
 import os
-
 from fpdf import FPDF
 import glob
 
@@ -7,22 +6,20 @@ arr_jpg = [f for f in glob.glob("pic*.jpg")]
 imageList = sorted(arr_jpg)
 
 waveFile = 'wave.png'
-
 pdf = FPDF(orientation='l')
 
-pdf.set_font('Arial', 'B', 16)
+pdf.add_font('sysfont', 'B', r"PTSansRegular.ttf", uni=True)
+pdf.set_font('sysfont', 'B', 16)
 
 counter = 0
 name_pict = "pic"
 name_form = ".jpg.txt"
+
 for imageFile in imageList:
     counter = counter + 1
     pdf.add_page()
-
     pdf.image(waveFile, 0, 0, 297, 20)
     where_is_config = os.path.exists(name_pict + str(counter) + name_form)
-    print(where_is_config)
-    print((name_pict + str(counter) + name_form))
     if where_is_config:
         txt_content = "PHOTO #" + str(counter) + " | " + open(imageFile + ".txt", "r").read()
     else:
